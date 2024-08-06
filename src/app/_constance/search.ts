dfee03ab4ed9d21131bf41991b30f844340d91ba
Edison Vitor <edison.vitor@hotmail.com>
2024-08-06 17:27:36 -0300

    feat: :lipstick: add Footer and BulkingItem component

---
+++ src/app/_constance/search.ts
@@ -0,0 +1,33 @@
+interface QuickSearchOptions {
+  imageUrl: string;
+  title: string;
+}
+
+const quickSearchOptions: QuickSearchOptions[] = [
+  {
+    title: "Cabelo",
+    imageUrl: "/corte.svg",
+  },
+  {
+    title: "Barba",
+    imageUrl: "/barba.svg",
+  },
+  {
+    title: "Acabamento",
+    imageUrl: "/acabamento.svg",
+  },
+  {
+    title: "Hidratação",
+    imageUrl: "/hidratacao.svg",
+  },
+  {
+    title: "Sombrancelha",
+    imageUrl: "/sombrancelha.svg",
+  },
+  {
+    title: "Massagem",
+    imageUrl: "/massagem.svg",
+  },
+];
+
+export default quickSearchOptions;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,16 @@ import { Badge } from "./_components/ui/badge";
 import { Avatar, AvatarImage } from "./_components/ui/avatar";
 import { db } from "./_lib/prisma";
 import BarberShopItem from "./_components/barber-shop-item";
+import quickSearchOptions from "./_constance/search";
+import BulkingItem from "./_components/Bulking-tem";
 
 export default async function Home() {
   const barbershops = await db.barbershop.findMany({});
+  const popularBarbershops = await db.barbershop.findMany({
+    orderBy: {
+      name: "desc",
+    },
+  });
 
   return (
     <div>
@@ -25,6 +32,19 @@ export default async function Home() {
               <SearchIcon />
             </Button>
           </div>
+          <div className="mt-6 flex gap-3 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
+            {quickSearchOptions.map((option) => (
+              <Button key={option.title} className="gap-2" variant="secondary">
+                <Image
+                  alt={option.title}
+                  src={option.imageUrl}
+                  width={16}
+                  height={16}
+                />
+                {option.title}
+              </Button>
+            ))}
+          </div>
           <div className="relative mt-6 h-[150px] w-full">
             <Image
               alt="Agende nos melhores com FWS Barber"
@@ -34,28 +54,7 @@ export default async function Home() {
             />
           </div>
         </div>
-        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
-          agendamentos
-        </h2>
-        <Card>
-          <CardContent className="flex justify-between p-0">
-            <div className="flex flex-col gap-2 py-5 pl-5">
-              <Badge>Confirmado</Badge>
-              <h3 className="text-xl font-semibold">Corte de cabelo</h3>
-              <div className="flex items-center gap-2">
-                <Avatar className="h-6 w-6">
-                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
-                </Avatar>
-                <p className="text-sm">Vintage Barber</p>
-              </div>
-            </div>
-            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
-              <p className="text-sm">Agosto</p>
-              <p className="text-2xl">06</p>
-              <p className="text-sm">14:00</p>
-            </div>
-          </CardContent>
-        </Card>
+        <BulkingItem />
         <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
           agendamentos
         </h2>
@@ -64,7 +63,24 @@ export default async function Home() {
             <BarberShopItem key={barbershop.id} barbershop={barbershop} />
           ))}
         </div>
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          populares
+        </h2>
+        <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
+          {popularBarbershops.map((barbershop) => (
+            <BarberShopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
       </div>
+      <footer>
+        <Card>
+          <CardContent className="px-5 py-6">
+            <p className="text-sm text-gray-400">
+              © 2023 Copyright <span className="font-bold">FSW Barber</span>
+            </p>
+          </CardContent>
+        </Card>
+      </footer>
     </div>
   );
 }
