ff2d6918dae84d479292dee20ed48704e8feb147
Edison Vitor <edison.vitor@hotmail.com>
2024-08-06 11:48:18 -0300

    feat: :lipstick: add banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,31 @@
 import Image from "next/image";
 import Header from "./_components/Header";
+import { Input } from "./_components/ui/input";
+import { Button } from "./_components/ui/button";
+import { SearchIcon } from "lucide-react";
 
 export default function Home() {
-  return <Header />;
+  return (
+    <div>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Ola, Miguel!</h2>
+        <p>Terça, 6 de Agosto</p>
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Buscar..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            alt="Agende nos melhores com FWS Barber"
+            fill
+            className="rounded-xl object-cover"
+            src="/banner.png"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
