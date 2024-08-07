efb29967d464e1b48f9ade058fb4ea5dd5eb7d39
Edison Vitor <edison.vitor@hotmail.com>
2024-08-07 02:51:33 -0300

    feat: :lipstick: add a barbershop page and barbershop service in barbershop pages

---
+++ src/app/barbershop/[id]/page.tsx
@@ -0,0 +1,80 @@
+import ServiceItem from "@/app/_components/ServiceItem";
+import { Button } from "@/app/_components/ui/button";
+import { Card, CardContent } from "@/app/_components/ui/card";
+import { db } from "@/app/_lib/prisma";
+import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { notFound } from "next/navigation";
+
+interface BarbershopPageProps {
+  params: {
+    id: string;
+  };
+}
+
+const BarbashopPage = async ({ params }: BarbershopPageProps) => {
+  const barbershop = await db.barbershop.findUnique({
+    where: {
+      id: params.id,
+    },
+    include: {
+      services: true,
+    },
+  });
+  if (!barbershop) {
+    return notFound();
+  }
+  return (
+    <div>
+      <div className="relative h-[250px] w-full">
+        <Image
+          className="object-cover"
+          alt={barbershop?.name}
+          fill
+          src={barbershop?.image_url}
+        />
+        <Button
+          size="icon"
+          variant="secondary"
+          className="absolute left-4 top-4"
+          asChild
+        >
+          <Link href="/">
+            <ChevronLeftIcon />
+          </Link>
+        </Button>
+        <Button
+          size="icon"
+          variant="secondary"
+          className="absolute right-4 top-4"
+        >
+          <MenuIcon />
+        </Button>
+      </div>
+      <div className="border-b border-solid p-5">
+        <h1 className="mb-3 text-xl font-bold">{barbershop?.name}</h1>
+        <div className="mb-2 flex items-center gap-1">
+          <MapPinIcon className="text-primary" size={18} />
+          <p className="text-sm">{barbershop?.address}</p>
+        </div>
+        <div className="flex items-center gap-1">
+          <StarIcon className="fill-primary text-primary" size={18} />
+          <p className="text-sm">{barbershop?.address}</p>
+        </div>
+      </div>
+      <div className="space-y-3 border-b border-solid p-5">
+        <h2 className="text-xs font-bold uppercase text-gray-400">Sobre nós</h2>
+        <p className="text-sm">{barbershop?.description}</p>
+      </div>
+      <div className="space-y-3 border-b border-solid p-5">
+        <h2 className="text-xs font-bold uppercase text-gray-400">Serviços</h2>
+        {barbershop.services.map((service) => (
+          <ServiceItem key={service.id} service={service} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default BarbashopPage;
