659af6445efd4ad6cf234619ad1585719aa00c77
Edison Vitor <edison.vitor@hotmail.com>
2024-08-08 11:18:21 -0300

    chore: add next-auth setup

--- src/app/_components/Hamburger-menu.tsx
+++ src/app/_components/Hamburger-menu.tsx
@@ -1,4 +1,10 @@
-import { CalendarIcon, HomeIcon, LogOutIcon, MenuIcon } from "lucide-react";
+import {
+  CalendarIcon,
+  HomeIcon,
+  LogInIcon,
+  LogOutIcon,
+  MenuIcon,
+} from "lucide-react";
 import { Button } from "./ui/button";
 import {
   Sheet,
@@ -12,6 +18,14 @@ import { Avatar, AvatarImage } from "./ui/avatar";
 import Link from "next/link";
 import quickSearchOptions from "../_constance/search";
 import Image from "next/image";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
 
 const HamburgerMenu = () => {
   return (
@@ -25,14 +39,39 @@ const HamburgerMenu = () => {
         <SheetHeader>
           <SheetTitle className="text-left">Menu</SheetTitle>
         </SheetHeader>
-        <div className="flex items-center gap-3 border-b border-solid py-5">
-          <Avatar>
+        <div className="flex items-center justify-between gap-3 border-b border-solid py-5">
+          <h2 className="font-bold">Ola, faça seu login!</h2>
+          <Dialog>
+            <DialogTrigger>
+              <Button size="icon">
+                <LogInIcon />
+              </Button>
+            </DialogTrigger>
+            <DialogContent className="w-[80%]">
+              <DialogHeader>
+                <DialogTitle>Faça login na plataforma</DialogTitle>
+                <DialogDescription>
+                  Conecte-se usando sua conta do Google
+                </DialogDescription>
+              </DialogHeader>
+              <Button variant="outline" className="gap-1 font-bold">
+                <Image
+                  src="/Google.svg"
+                  alt="fazer login com o google"
+                  width={18}
+                  height={18}
+                />
+                Google
+              </Button>
+            </DialogContent>
+          </Dialog>
+          {/* <Avatar>
             <AvatarImage src="https://plus.unsplash.com/premium_photo-1664536392779-049ba8fde933?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fGF2YXRhcnxlbnwwfHwwfHx8MA%3D%3D" />
           </Avatar>
           <div>
             <p className="font-bold">Guilherme Rocha</p>
             <p className="text-sm">guilherme@exemplo.com</p>
-          </div>
+          </div> */}
         </div>
         <div className="flex flex-col gap-2 border-b border-solid py-5">
           <SheetClose asChild>
