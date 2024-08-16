26414ae0308282412da08204d2143c048c0484b1
Edison Vitor <edison.vitor@hotmail.com>
2024-08-16 03:57:18 -0300

    fix: :adhesive_bandage: fix logo  src path

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -9,7 +9,7 @@ const Header = () => {
       <CardContent className="flex flex-row items-center justify-between p-5">
         <Link href="/">
           <Image
-            src="/logo.svg"
+            src="/Logo.svg"
             alt="BarberShop Logo"
             width={120}
             height={18}
